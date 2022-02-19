2af2db31d2849cc433abda73ece2a2a815f2239c
Iaggo Capitanio <49845024+iaggocapitanio1@users.noreply.github.com>
2022-02-19 22:30:12 +0000

    whitenoise

--- compig/settings.py
+++ compig/settings.py
@@ -169,7 +169,6 @@ AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' # 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' # 'storages.backends.s3boto3.S3StaticStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
